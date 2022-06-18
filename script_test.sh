21cc83b88027a668f56d75245a0907ee14d61938
Boughanmi Rami <rami.boughanmi@etu.u-pec.fr>
2022-06-18 10:40:30 +0200

    first commit

---
+++ script_test.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+string="unzip_"
+
+path = "/home/osint/Documents/BDD_r_2/undefined/VolumePurcharses/"
+echo "$path"
+
+
+echo  ${#string}
+echo ${string:${#string}-3:${#string}}
+
+if [[ "a" = "a" ]]
+then
+	echo "sa marche enculer"
+fi
+
+x =$1
+
+
+for i in *;
+do
+	if [[ ${i:${#i}-3:${#i}} = "zip" ]];
+	then
+		x=$(( $x + 1))
+		#mkdir "$string"$x
+		unzip $i -d  "/home/osint/Documents/BDD_r_2/undefined/VolumePurchases/$string"$x
+		rm $i
+		#echo "Working on $i file $ ";
+	fi
+done
